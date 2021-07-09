05a8ab4c35eaf7ecabcd2551452042f27d3534e1
junerae <80211043+junerae@users.noreply.github.com>
2021-07-09 08:27:58 +0200

    Add files via upload

---
+++ Opdracht01_GebruikJeEersteJavascript/index.js
@@ -0,0 +1,27 @@
+console.log("Hello Winc Academy")
+let firstName = 'June';
+console.log("June");
+
+let sum1 = 4 + 4;
+console.log(sum1);
+
+let sum2 = 4 - 4;
+console.log(sum2);
+
+let sum3 = 4 * 4;
+console.log(sum3);
+
+let sum4 = 4 / 4;
+console.log(sum4);
+
+let sum5 = 4 % 4;
+console.log(sum5);
+
+let age = 33;
+console.log(typeof age);
+
+let ageText = '33';
+console.log(typeof ageText);
+
+
+
